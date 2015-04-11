670595de6f8f46b5ac3c72a0fe236220241c2379
Ahmed Mostafa <amostafa88@gmail.com>
2015-04-11 18:24:40 +0300

    None

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into book(book_name) values('The Hitory of Man Kind');
+insert into book(book_name) values('Ware and Peace');
+insert into book(book_name) values('My Strugle');
+insert into book(book_name) values('The Secret');
+insert into book(book_name) values('100 Years of Lonlyness');
+
